88e688ef542daf888b628eae99b297c431bcb6eb
Elmehdi <eaitbrahim@gmail.com>
2020-02-19 12:54:23 +0100

    Fetch next generation i to the ui

---
+++ frontend/src/components/Generation.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+
+const DEFAULT_GENERATION = { generationId: '', expiration: '' };
+const MINIMUM_DELAY = 3000;
+
+class Generation extends Component {
+  state = { generation: DEFAULT_GENERATION };
+
+  fetchGeneration = () => {
+    fetch('http://localhost:3000/generation')
+      .then(response => response.json())
+      .then(json => {
+        this.setState({ generation: json.generation });
+      })
+      .catch(error => console.error('error:', error));
+  };
+
+  fetchNextGeneration = () => {
+    this.fetchGeneration();
+    let delay =
+      new Date(this.state.generation.expiration).getTime() -
+      new Date().getTime();
+    if (delay < MINIMUM_DELAY) delay = delay;
+
+    setTimeout(() => this.fetchNextGeneration(), delay);
+  };
+
+  componentDidMount() {
+    this.fetchNextGeneration();
+  }
+
+  render() {
+    const { generation } = this.state;
+    return (
+      <div>
+        <h3>Generation {generation.generationId}. Expires on:</h3>
+        <h4>{new Date(generation.expiration).toString()}</h4>
+      </div>
+    );
+  }
+}
+
+export default Generation;
